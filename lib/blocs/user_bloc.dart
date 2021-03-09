b25926faf04dc31bccf7d46cdb2800790a03e871
brenosekiro <b88660501@gmail.com>
2021-03-09 18:14:39 -0300

    totalSpent

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -22,16 +22,44 @@ class UserBloc implements BlocBase {
         switch(change.type) {
           case DocumentChangeType.added:
             _users[uid] = change.doc.data();
+            _subscribeToOrders(uid);
             break;
           case DocumentChangeType.modified:
             _users[uid].addAll(change.doc.data());
+            _usersController.add(_users.values.toList());
             break;
           case DocumentChangeType.removed:
             _users.remove(uid);
+            _unsubscribeToOrders(uid);
+            _usersController.add(_users.values.toList());
             break;
         }
       });
     });
+  }
+
+  void _subscribeToOrders(String uid) {
+    _users[uid]['subscription'] = _firestore.collection('users')
+      .doc(uid).collection('orders').snapshots()
+      .listen((orders) async {
+        int numOrders = orders.docs.length;
+        double money = 0.0;
+        for(DocumentSnapshot d in orders.docs) {
+          DocumentSnapshot order = await _firestore.collection('orders').doc(d.id).get();
+
+          if(order.data() == null) continue;
+
+          money += order.data()['totalPrice'];
+        }
+
+        _users[uid].addAll({ "money": money, "orders": numOrders });
+
+        _usersController.add(_users.values.toList());
+      });
+  }
+
+  void _unsubscribeToOrders(String uid) {
+    _users[uid]['subscription'].cancel();
   }
   
   @override
