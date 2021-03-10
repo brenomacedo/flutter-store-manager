114c9c70685a906be223cefb7f6a138727e1df56
brenosekiro <b88660501@gmail.com>
2021-03-10 05:15:54 -0300

    showing registered products

--- lib/widgets/category_tile.dart
+++ lib/widgets/category_tile.dart
@@ -19,6 +19,39 @@ class CategoryTile extends StatelessWidget {
             backgroundImage: NetworkImage(category.data()['icon']),
             backgroundColor: Colors.transparent,
           ),
+          children: [
+            FutureBuilder<QuerySnapshot>(
+              future: category.reference.collection('items').get(),
+              builder: (context, snapshot) {
+                if(!snapshot.hasData)
+                  return Container();
+                return Column(
+                  children: snapshot.data.docs.map((doc) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(doc.data()['images'][0]),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      title: Text(doc.data()['title']),
+                      trailing: Text('R\$${doc.data()['price'].toStringAsFixed(2)}'),
+                      onTap: () {
+
+                      },
+                    );
+                  }).toList()..add(
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        child: Icon(Icons.add, color: Colors.pinkAccent),
+                      ),
+                      title: Text('Adicionar'),
+                      onTap: () {},
+                    )
+                  ),
+                );
+              }
+            )
+          ],
         ),
       ),
     );
