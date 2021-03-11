426959051466f418e2e2d6576721a2519f1d5930
brenosekiro <b88660501@gmail.com>
2021-03-11 04:38:10 -0300

    product_screen

--- lib/product/product_screen.dart
+++ lib/product/product_screen.dart
@@ -1,24 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:loja/blocs/product_bloc.dart';
+import 'package:loja/widgets/images_widget.dart';
 
-class ProductScreen extends StatelessWidget {
+
+class ProductScreen extends StatefulWidget {
 
   final String categoryId;
   final DocumentSnapshot product;
-  final ProductBloc _productBloc;
+  ProductScreen({ this.categoryId, this.product });
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState(categoryId, product);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+
   final _formKey = GlobalKey<FormState>();
 
-  ProductScreen({ this.categoryId, this.product }) :
+  final ProductBloc _productBloc;
+
+  _ProductScreenState(String categoryId, DocumentSnapshot product) :
     _productBloc = ProductBloc(product: product, categoryId: categoryId);
+   
 
   @override
   Widget build(BuildContext context) {
+
+    final fieldStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 16
+    );
+
+    InputDecoration fieldDecoration(String label) {
+      return InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.grey)
+      );
+    }
+
     return Scaffold(
       backgroundColor: Colors.grey[850],
       appBar: AppBar(
         elevation: 0,
-        title: Text('Criar produto'),
+        title: Text('Criar produsto'),
         actions: [
           IconButton(
             icon: Icon(Icons.remove),
@@ -32,12 +57,52 @@ class ProductScreen extends StatelessWidget {
       ),
       body: Form(
         key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: [
-            
-          ],
-        ),
+        child: StreamBuilder(
+          stream: _productBloc.outData,
+          builder: (context, snapshot) {
+
+            if(!snapshot.hasData) {
+              return Container();
+            }
+
+            return ListView(
+              padding: EdgeInsets.all(16),
+              children: [
+                Text('Imagens', style: TextStyle(color: Colors.grey[850], fontSize: 12)),
+                ImagesWidget(),
+                TextFormField(
+                  initialValue: snapshot.data['title'],
+                  style: fieldStyle,
+                  decoration: fieldDecoration('Título'),
+                  onSaved: (text) {
+
+                  },
+                  validator: null,
+                ),
+                TextFormField(
+                  initialValue: snapshot.data['description'],
+                  style: fieldStyle,
+                  decoration: fieldDecoration('Descrição'),
+                  maxLines: 6,
+                  onSaved: (text) {
+
+                  },
+                  validator: null,
+                ),
+                TextFormField(
+                  initialValue: snapshot.data['price']?.toStringAsFixed(2),
+                  style: fieldStyle,
+                  decoration: fieldDecoration('Preço'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  onSaved: (text) {
+
+                  },
+                  validator: null,
+                )
+              ],
+            );
+          },
+        )
       ),
     );
   }
